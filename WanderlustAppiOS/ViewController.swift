4b24d3189842a9a99e3a4d062f5622fe7d8297fd
aayushj567 <aayushjaiswal765@gmail.com>
2024-04-11 18:39:55 -0400

    added comments in view controller

--- WanderlustAppiOS/ViewController.swift
+++ WanderlustAppiOS/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var handleAuth: AuthStateDidChangeListenerHandle?
     // variable to keep an instance of the current signed-in Firebase user
     var currentUser:FirebaseAuth.User?
+    // flag to check if the user has completed the registration process
     var hasCompletedRegistration = false
 
     let loginScreen = LoginView()
@@ -21,33 +22,29 @@ class ViewController: UIViewController {
         view = loginScreen
     }
     
-    
+    //MARK: add the Firebase auth listener when the view appears...
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print(hasCompletedRegistration)
         handleAuth = Auth.auth().addStateDidChangeListener{ [weak self] auth, user in
             guard let self = self else { return }
-            if user != nil && !self.hasCompletedRegistration {
+            if user != nil && !self.hasCompletedRegistration { // already a user
                 let mainScreen = CalendarViewController();               self.navigationController?.pushViewController(mainScreen, animated: true)
-            } else if  user != nil && self.hasCompletedRegistration { // completed registration
+            } else if  user != nil && self.hasCompletedRegistration { // new user just completed registration
                     self.hasCompletedRegistration = false
                     return
             }
         }
-
     }
-
+    
+    //MARK: view did load...
     override func viewDidLoad() {
         super.viewDidLoad()
-
         navigationController?.navigationBar.prefersLargeTitles = true
         loginScreen.buttonLogin.addTarget(self, action: #selector(onLoginTapped), for: .touchUpInside)
         loginScreen.buttonRegister.addTarget(self, action: #selector(onRegistrationTapped), for: .touchUpInside)
-        
-                
     }
     
-    
+    //MARK: remove the Firebase auth listener when the view disappears...
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         Auth.auth().removeStateDidChangeListener(handleAuth!)
@@ -68,10 +65,8 @@ class ViewController: UIViewController {
         })
     }
     
-    
     // MARK: action when the login button is tapped...
     @objc func onLoginTapped(){
-       
         guard let emailText = loginScreen.textFieldEmail.text, !emailText.isEmpty,
               let passwordText = loginScreen.textFieldPassword.text, !passwordText.isEmpty
         else {
@@ -86,28 +81,25 @@ class ViewController: UIViewController {
         signIntoFirebase(email: emailText, password: passwordText)
     }
     
-    
-    // Comment added
+    //MARK: action when the registration button is tapped...
     @objc func onRegistrationTapped(){
-       
         let registrationScreen = RegistrationViewController()
         navigationController?.pushViewController(registrationScreen, animated: true)
     }
 
-    
+    //MARK: function to check if the email is valid...
     func isValidEmail(_ email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
     }
     
-    
+    //MARK: function to show an alert...
     func showAlert(title:String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
-    
 }
 
 
