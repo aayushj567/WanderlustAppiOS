1731d2d08bb4d105f749509a352689c5f5fa82f8
sriker99 <vootukuri.s@northeastern.edu>
2024-04-13 01:09:00 -0400

    chat feature working

---
+++ WanderlustAppiOS/configs.swift
@@ -0,0 +1,13 @@
+//
+//  configs.swift
+//  WanderlustAppiOS
+//
+//  Created by Sai Sriker Reddy Vootukuri on 4/12/24.
+//
+
+import Foundation
+
+class Configs{
+    static let tableViewUsersId = "tableViewUsersID"
+    static let tableViewMessagesID = "tableViewMessagesID"
+}
