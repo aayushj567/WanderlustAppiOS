f94cd205fa1e4e35b00ab614c3675e93d121ef32
sriker99 <vootukuri.s@northeastern.edu>
2024-04-15 22:45:12 -0400

    updated look and feel of chat

--- WanderlustAppiOS/Chat Feature/Main Screen/ChatPlanTableViewCell.swift
+++ WanderlustAppiOS/Chat Feature/Main Screen/ChatPlanTableViewCell.swift
@@ -2,26 +2,67 @@ import UIKit
 
 class ChatPlanTableViewCell: UITableViewCell {
     
-    let planNameLabel: UILabel = {
-        let label = UILabel()
-        // Customize label properties if needed
-        return label
-    }()
+    var wrapperCellView: UIView!
+    var planName: UILabel!
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        addSubview(planNameLabel)
-        planNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        setupWrapperCellView()
+        setupPlanLabel()
+        initConstraints()
+    }
+    
+    func setupWrapperCellView(){
+        wrapperCellView = UITableViewCell()
+        wrapperCellView.backgroundColor = .white
+        wrapperCellView.layer.cornerRadius = 6.0
+        wrapperCellView.layer.shadowColor = UIColor.gray.cgColor
+        wrapperCellView.layer.shadowOffset = .zero
+        wrapperCellView.layer.shadowRadius = 4.0
+        wrapperCellView.layer.shadowOpacity = 0.4
+        wrapperCellView.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.layer.borderColor = UIColor.green.cgColor
+        self.addSubview(wrapperCellView)
+    }
+    
+    func setupPlanLabel(){
+        planName = UILabel()
+        planName.translatesAutoresizingMaskIntoConstraints = false
+        wrapperCellView.addSubview(planName)
+    }
+
+    func initConstraints(){
         NSLayoutConstraint.activate([
-            planNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            planNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            planNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            planNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+            wrapperCellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            wrapperCellView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            wrapperCellView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            wrapperCellView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
+            
+            planName.topAnchor.constraint(equalTo: wrapperCellView.topAnchor, constant: 2),
+            planName.leadingAnchor.constraint(equalTo: wrapperCellView.leadingAnchor, constant: 6),
+            planName.centerYAnchor.constraint(equalTo: wrapperCellView.centerYAnchor),
+            planName.heightAnchor.constraint(equalToConstant: 10),
+            planName.widthAnchor.constraint(lessThanOrEqualTo: wrapperCellView.widthAnchor),
+            
+            wrapperCellView.heightAnchor.constraint(equalToConstant: 32)
         ])
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
 }
