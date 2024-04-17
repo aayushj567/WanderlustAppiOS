2e93b92648ad02bbf1c76f906d9f9f91f76d7bac
Imperium-Aneesh <imperium.kumar@gmail.com>
2024-04-16 22:59:39 -0400

    search guest implemented and fixed itinerary screen

--- WanderlustAppiOS/Add_Guests/AddGuestsViewController.swift
+++ WanderlustAppiOS/Add_Guests/AddGuestsViewController.swift
@@ -82,6 +82,88 @@
 //
 //
 
+//import UIKit
+//import FirebaseAuth
+//import FirebaseFirestore
+//
+//class AddGuestsViewController: UIViewController {
+//    
+//    let addGuestView = AddGuestsView()
+//    var users: [User] = []
+//    var selectedUsers: [User] = []
+//    var selectedDates: [Date] = []
+//    
+//    override func loadView() {
+//        view = addGuestView
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        title = "Add Guests"
+//        addGuestView.tableViewPeople.separatorStyle = .none
+//        addGuestView.tableViewPeople.separatorColor = .clear
+//        addGuestView.tableViewPeople.dataSource = self
+//        addGuestView.tableViewPeople.delegate = self
+//        addGuestView.buttonNext.addTarget(self, action: #selector(onNextButtonTapped), for: .touchUpInside)
+//        fetchUsers()
+//    }
+//    
+//    func fetchUsers() {
+//        let db = Firestore.firestore()
+//        db.collection("users").getDocuments { [weak self] (querySnapshot, error) in
+//            guard let self = self else { return }
+//            if let error = error {
+//                print("Error getting users: \(error)")
+//            } else {
+//                self.users = querySnapshot?.documents.compactMap { document in
+//                    try? document.data(as: User.self)
+//                } ?? []
+//                self.addGuestView.tableViewPeople.reloadData()
+//            }
+//        }
+//    }
+//    
+//    @objc func onNextButtonTapped() {
+//        let itineraryViewController = ItineraryViewController()
+//        itineraryViewController.selectedDates = selectedDates
+//        itineraryViewController.selectedUsers = selectedUsers
+//        navigationController?.pushViewController(itineraryViewController, animated: true)
+//    }
+//}
+//
+//extension AddGuestsViewController: UITableViewDelegate, UITableViewDataSource {
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return users.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "addGuestPeople", for: indexPath) as! AddPeopleTableViewCell
+//        let user = users[indexPath.row]
+//        cell.personNameLabel.text = user.name
+//        
+//        // Set the initial state of the checkbox based on whether the user is selected or not
+//        cell.checkboxButton.isSelected = selectedUsers.contains(where: { $0.id == user.id })
+//        
+//        // Pass the user tapped closure to handle checkbox state changes
+//        cell.userTapped = { [weak self] isSelected in
+//            guard let self = self else { return }
+//            if isSelected {
+//                self.selectedUsers.append(user)
+//            } else {
+//                self.selectedUsers.removeAll(where: { $0.id == user.id })
+//            }
+//        }
+//        
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//    }
+//}
+
+
 import UIKit
 import FirebaseAuth
 import FirebaseFirestore
@@ -90,6 +172,7 @@ class AddGuestsViewController: UIViewController {
     
     let addGuestView = AddGuestsView()
     var users: [User] = []
+    var filteredUsers: [User] = []
     var selectedUsers: [User] = []
     var selectedDates: [Date] = []
     
@@ -105,6 +188,7 @@ class AddGuestsViewController: UIViewController {
         addGuestView.tableViewPeople.dataSource = self
         addGuestView.tableViewPeople.delegate = self
         addGuestView.buttonNext.addTarget(self, action: #selector(onNextButtonTapped), for: .touchUpInside)
+        addGuestView.searchBar.delegate = self
         fetchUsers()
     }
     
@@ -118,6 +202,7 @@ class AddGuestsViewController: UIViewController {
                 self.users = querySnapshot?.documents.compactMap { document in
                     try? document.data(as: User.self)
                 } ?? []
+                self.filteredUsers = self.users
                 self.addGuestView.tableViewPeople.reloadData()
             }
         }
@@ -134,18 +219,16 @@ class AddGuestsViewController: UIViewController {
 extension AddGuestsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return filteredUsers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "addGuestPeople", for: indexPath) as! AddPeopleTableViewCell
-        let user = users[indexPath.row]
+        let user = filteredUsers[indexPath.row]
         cell.personNameLabel.text = user.name
         
-        // Set the initial state of the checkbox based on whether the user is selected or not
         cell.checkboxButton.isSelected = selectedUsers.contains(where: { $0.id == user.id })
         
-        // Pass the user tapped closure to handle checkbox state changes
         cell.userTapped = { [weak self] isSelected in
             guard let self = self else { return }
             if isSelected {
@@ -162,3 +245,15 @@ extension AddGuestsViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
+extension AddGuestsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredUsers = searchText.isEmpty ? users : users.filter { $0.name?.lowercased().contains(searchText.lowercased()) ?? false }
+        addGuestView.tableViewPeople.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
+
--- WanderlustAppiOS/Itinerary Screen/ItineraryViewController.swift
+++ WanderlustAppiOS/Itinerary Screen/ItineraryViewController.swift
@@ -14,97 +14,76 @@ struct Day: Codable {
     }
 }
 
+
 class ItineraryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var tableView: UITableView!
-    var startDatePicker: UIDatePicker!
-    var endDatePicker: UIDatePicker!
+    var itineraryView: ItineraryView!
     var days: [Day] = []
-    var saveButton : UIButton!
-    var tabBarView: UIView!
     var onIconTapped: ((Int) -> Void)?
-    var planNameLabel:UILabel!
+    var selectedDates: [Date] = []
+    var selectedUsers: [User] = []
     var destinationIds: [String] = []
-
+    
+    override func loadView() {
+        itineraryView = ItineraryView(frame: UIScreen.main.bounds)
+        view = itineraryView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        updateDaysFromDates() // Initial setup for the days based on the date range
-        saveButton.addTarget(self, action: #selector(saveItinerary), for: .touchUpInside)
+        itineraryView.tableView.delegate = self
+        itineraryView.tableView.dataSource = self
+        itineraryView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "dayCell")
+        
+        itineraryView.startDatePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+        itineraryView.endDatePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+        itineraryView.saveButton.addTarget(self, action: #selector(saveItinerary), for: .touchUpInside)
+        
+        updateDaysFromDates()
+        setupTabBarActions()
+        print("Selected Users: \(selectedUsers)")
     }
-
-    func setupUI() {
-        view.backgroundColor = .white
-        
-        planNameLabel = UILabel()
-        planNameLabel.text = "Your Plan Name"
-        planNameLabel.textAlignment = .center
-        planNameLabel.font = UIFont.boldSystemFont(ofSize: 20)
-        planNameLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        // Start Date Picker
-        
-        startDatePicker = UIDatePicker()
-        startDatePicker.datePickerMode = .date
-        startDatePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
-        startDatePicker.translatesAutoresizingMaskIntoConstraints = false
-
-        // End Date Picker
-        endDatePicker = UIDatePicker()
-        endDatePicker.datePickerMode = .date
-        endDatePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
-        endDatePicker.translatesAutoresizingMaskIntoConstraints = false
-
-        // Setting up the tableView
-        tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "dayCell")
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-
-        
-        saveButton = UIButton()
-        saveButton.translatesAutoresizingMaskIntoConstraints = false
-        saveButton.setTitle("Save", for: .normal)
-        saveButton.backgroundColor = .systemBlue // Example color
-        saveButton.layer.cornerRadius = 5
-        setupTabBarView()
-        view.addSubview(planNameLabel)
-        view.addSubview(saveButton)
-        // Add subviews
-        view.addSubview(tableView)
-        view.addSubview(startDatePicker)
-        view.addSubview(endDatePicker)
-        view.addSubview(saveButton)
-
-        // Apply Auto Layout
-        applyConstraints()
+    
+    func setupTabBarActions() {
+        for subview in itineraryView.tabBarView.subviews {
+            if let stackView = subview as? UIStackView {
+                for iconView in stackView.arrangedSubviews {
+                    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tabBarIconTapped(_:)))
+                    iconView.addGestureRecognizer(tapGesture)
+                }
+            }
+        }
     }
+    
+    @objc func tabBarIconTapped(_ sender: UITapGestureRecognizer) {
+        guard let iconView = sender.view else { return }
+        let index = iconView.tag
+        onIconTapped?(index)
+    }
+    
     @objc func saveItinerary() {
         guard let currentUser = Auth.auth().currentUser else {
             fatalError("No user signed in")
         }
-
+        
         let db = Firestore.firestore()
         let plansRef = db.collection("plans")
         let planDocument = plansRef.document()
-
+        
         var newPlan = Plan(
-            name: planNameLabel.text ?? "Untitled Plan",
-            dateFrom: startDatePicker.date,
-            dateTo: endDatePicker.date,
+            name: itineraryView.planNameLabel.text ?? "Untitled Plan",
+            dateFrom: selectedDates.first ?? Date(),
+            dateTo: selectedDates.last ?? Date(),
             dayIds: [],
             owner: currentUser.uid
-            
         )
-
+        
         do {
             try planDocument.setData(from: newPlan) { error in
                 if let error = error {
                     print("Error writing document: \(error)")
                 } else {
                     print("Plan successfully saved!")
-                    // After saving the plan, proceed to save the days.
                     self.saveDays(planId: planDocument.documentID, planDocument: planDocument)
                     let nextScreen = MyPlansViewController()
                     self.navigationController?.pushViewController(nextScreen, animated: true)
@@ -114,17 +93,18 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
             print("Error serializing plan: \(error)")
         }
     }
+    
     func saveDays(planId: String, planDocument: DocumentReference) {
         let db = Firestore.firestore()
         let daysRef = db.collection("days")
-
+        
         var dayIds: [String] = []
-
+        
         for day in days {
             let dayDocument = daysRef.document()
             var dayCopy = day
-            dayCopy.planId = planId  // Assign planId to day
-
+            dayCopy.planId = planId
+            
             do {
                 try dayDocument.setData(from: dayCopy) { error in
                     if let error = error {
@@ -132,17 +112,14 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
                     } else {
                         print("Day \(day.name) successfully saved!")
                         dayIds.append(dayDocument.documentID)
-                        // Save destinations for this day and update day with destination IDs
-                        self.saveDestinations(dayId: dayDocument.documentID, dayDocument: dayDocument, destinations: day.destinations!)
+                        self.saveDestinations(dayId: dayDocument.documentID, dayDocument: dayDocument, destinations: day.destinations ?? [])
                     }
                 }
             } catch let error {
                 print("Error serializing day: \(error)")
             }
-            
         }
-
-        // Update the plan document with the day IDs after all days are saved
+        
         planDocument.updateData(["dayIds": dayIds]) { error in
             if let error = error {
                 print("Error updating plan with day IDs: \(error)")
@@ -151,19 +128,16 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
             }
         }
     }
-
-
+    
     func saveDestinations(dayId: String, dayDocument: DocumentReference, destinations: [Destination]) {
         let db = Firestore.firestore()
         let destinationsRef = db.collection("destinations")
-
         
-
         for destination in destinations {
             let destinationDocument = destinationsRef.document()
             var destinationCopy = destination
-            destinationCopy.dayId = dayId  // Assign dayId to destination
-
+            destinationCopy.dayId = dayId
+            
             do {
                 try destinationDocument.setData(from: destinationCopy) { error in
                     if let error = error {
@@ -178,8 +152,7 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
                 print("Error serializing destination: \(error)")
             }
         }
-
-        // Update the day document with the destination IDs after all destinations are saved
+        
         print(self.destinationIds)
         dayDocument.updateData(["destinationIds": self.destinationIds]) { error in
             if let error = error {
@@ -189,144 +162,52 @@ class ItineraryViewController: UIViewController, UITableViewDelegate, UITableVie
             }
         }
     }
-
-    func setupTabBarView() {
-        tabBarView = UIView()
-        tabBarView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(tabBarView)
-
-        // Create a stack view for the icons
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.distribution = .fillEqually // This will distribute the space equally among the icons
-        stackView.alignment = .center
-        stackView.axis = .horizontal
-        tabBarView.addSubview(stackView)
-        
-        // Add constraints to the stack view
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: tabBarView.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: tabBarView.trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: tabBarView.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: tabBarView.bottomAnchor)
-        ])
-        
-        // Initialize icon views and add them to the stack view
-        let iconNames = ["house", "list.bullet", "message", "person.crop.circle"]
-        for (index, iconName) in iconNames.enumerated() {
-            let iconImageView = UIImageView(image: UIImage(systemName: iconName))
-            iconImageView.contentMode = .scaleAspectFit
-            iconImageView.isUserInteractionEnabled = true
-            iconImageView.tag = index  // Set the tag to the index of the iconName
-
-            // Add a gesture recognizer to each icon
-            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tabBarIconTapped(_:)))
-            iconImageView.addGestureRecognizer(tapGesture)
-            
-            stackView.addArrangedSubview(iconImageView)
-        }
-    }
-
-    @objc func tabBarIconTapped(_ sender: UITapGestureRecognizer) {
-            guard let iconView = sender.view else { return }
-            let index = iconView.tag
-            // Handle the icon tap based on the index
-            print("Icon at index \(index) was tapped.")
-                // Handle the icon tap, switch views accordingly
-                if(index == 0){
-                    let homeView = CalendarViewController()
-                    navigationController?.pushViewController(homeView, animated: true)
-                }
-                if(index == 1){
-                    let myplansVC = MyPlansViewController()
-                    navigationController?.pushViewController(myplansVC, animated: true)
-                }
-                if(index == 2)
-                {
-                    let chatView = ChatPlanViewController()
-                    navigationController?.pushViewController(chatView, animated: true)
-                }
-                print("Icon at index \(index) was tapped.")
-                if(index == 3)
-                {
-                    let profileView = ShowProfileViewController()
-                    navigationController?.pushViewController(profileView, animated: true)
-                }
-                guard let iconView = sender.view else { return }
-                //let index = iconView.tag
-                // The view controller that holds this view will set this closure to handle the icon tap.
-                onIconTapped?(index)
-            }
-
-    func applyConstraints() {
-        NSLayoutConstraint.activate([
-            planNameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 4),
-            planNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            planNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            startDatePicker.topAnchor.constraint(equalTo: planNameLabel.bottomAnchor, constant: 20),
-            startDatePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            endDatePicker.topAnchor.constraint(equalTo: startDatePicker.bottomAnchor, constant: 20),
-            endDatePicker.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            tableView.topAnchor.constraint(equalTo: endDatePicker.bottomAnchor, constant: 20),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: tabBarView.topAnchor, constant: -8),
-            tabBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tabBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tabBarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            tabBarView.heightAnchor.constraint(equalToConstant: 50),
-            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            saveButton.bottomAnchor.constraint(equalTo: tabBarView.topAnchor, constant: -10),
-            saveButton.heightAnchor.constraint(equalToConstant: 50)
-            
-        ])
-    }
-
+    
     @objc func dateChanged(_ sender: UIDatePicker) {
         updateDaysFromDates()
     }
-
+    
     func updateDaysFromDates() {
-        let start = startDatePicker.date
-        let end = endDatePicker.date
-        
-        // Ensure the end date is not earlier than the start date
-        if end >= start {
-            let calendar = Calendar.current
-            let dateRange = calendar.dateComponents([.day], from: start, to: end).day ?? 0
-            days = (0...dateRange).map { Day(name: "Day \($0 + 1)", destinations: []) }
-        } else {
-            days = []  // Clear days if the end date is before the start date
+        guard let startDate = selectedDates.first, let endDate = selectedDates.last else {
+            days = []
+            itineraryView.tableView.reloadData()
+            return
         }
-        tableView.reloadData()
+        
+        let calendar = Calendar.current
+        let dateRange = calendar.dateComponents([.day], from: startDate, to: endDate).day ?? 0
+        
+        if dateRange >= 0 {
+            days = (0...dateRange).map { Day(name: "Day \($0 + 1)", destinationIds: [], planId: nil, destinations: []) }
+        } else {
+            let swappedDateRange = calendar.dateComponents([.day], from: endDate, to: startDate).day ?? 0
+            days = (0...swappedDateRange).map { Day(name: "Day \($0 + 1)", destinationIds: [], planId: nil, destinations: []) }
+        }
+        
+        itineraryView.tableView.reloadData()
     }
-
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return days.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "dayCell", for: indexPath)
         let day = days[indexPath.row]
-        // Using newline to separate destination names instead of commas
-        let destinationDetails = day.destinations?.map { "\($0.name) - Duration: \($0.duration)" }.joined(separator: "\n") ?? "No destinations available"
-
-            cell.textLabel?.text = "\(day.name):\n\(destinationDetails)"
-            cell.textLabel?.numberOfLines = 0  // Allow unlimited lines for text label
-            cell.accessoryType = .detailDisclosureButton
+        let destinationDetails = day.destinations?.map { "\($0.name) - Duration: \($0.duration ?? "")" }.joined(separator: "\n") ?? ""
+        cell.textLabel?.text = "\(day.name):\n\(destinationDetails)"
+        cell.textLabel?.numberOfLines = 0
+        cell.accessoryType = .detailDisclosureButton
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let searchVC = SearchDestinationController()
         searchVC.onDestinationsSelected = { [weak self] selectedDestinations in
             guard let self = self else { return }
             self.days[indexPath.row].destinations = selectedDestinations
-            self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            self.itineraryView.tableView.reloadRows(at: [indexPath], with: .automatic)
         }
         navigationController?.pushViewController(searchVC, animated: true)
     }
 }
-
